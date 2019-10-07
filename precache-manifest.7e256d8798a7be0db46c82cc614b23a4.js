fe681e614e2e993431d30511240be01a3e0dedde
Vasyl Krupka <krupka.ua@gmail.com>
2019-10-07 13:58:13 +0300

    Updates

---
+++ precache-manifest.7e256d8798a7be0db46c82cc614b23a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23671e776477fd715b80273504b7762",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "383f3203ab4ab56a7e19",
+    "url": "/goit-react-hw-01-components/static/css/main.99c5c123.chunk.css"
+  },
+  {
+    "revision": "97932ca20203e3931a53",
+    "url": "/goit-react-hw-01-components/static/js/2.a1c5f467.chunk.js"
+  },
+  {
+    "revision": "383f3203ab4ab56a7e19",
+    "url": "/goit-react-hw-01-components/static/js/main.6a744639.chunk.js"
+  },
+  {
+    "revision": "6e529e5db644c6f2c309",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.4f1ae2cf.js"
+  }
+]);
